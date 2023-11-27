2e912fcacf92709e93ac5445964e1716b202038b
omergorenn <omergrn25@gmail.com>
2023-11-27 22:44:08 +0300

    URL-SHORTENER WITH REDIS AND FIBER

--- api/database/database.go
+++ api/database/database.go
@@ -1,1 +1,18 @@
 package database
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	"os"
+)
+
+var Ctx = context.Background()
+
+func CreateClient(dbNo int) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("DB_ADDRESS"),
+		Password: os.Getenv("DB_PASS"),
+		DB:       dbNo,
+	})
+	return rdb
+}
--- api/main.go
+++ api/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
-	"url-shortener/api/routes"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"url-shortener/routes"
 )
 
+// setup two routes, one for shortening the url
+// the other for resolving the url
+// for example if the short is `4fg`, the user
+// must navigate to `localhost:3000/4fg` to redirect to
+// original URL. The domain can be changes in .env file
 func setupRoutes(app *fiber.App) {
 	app.Get("/:url", routes.ResolveURL)
 	app.Post("/api/v1", routes.ShortenURL)
-
 }
 
 func main() {
 	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
 	app := fiber.New()
 
+	//app.Use(csrf.New())
+	app.Use(logger.New())
+
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
 }
